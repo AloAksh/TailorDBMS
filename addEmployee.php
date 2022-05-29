ad73c76b76a35b41cd8129e73a86d72c036aa613
elizzer <64605932+elizzer@users.noreply.github.com>
2022-05-29 13:31:16 +0530

    add Employee

--- addEmployee.php
+++ addEmployee.php
@@ -1,12 +1,5 @@
 <?php
-    include ("connect.php");
-
-    // if ($result = $mysqli -> query($sql)) {
-    //     while ($row = $result -> fetch_row()) {
-    //       printf ("%s (%s)\n", $row[0], $row[1]);
-    //     }
-    //     $result -> free_result();
-    //   }
+    include("connect.php")
 ?>
 
 <!DOCTYPE html>
@@ -18,23 +11,24 @@
     <title>Add Employe</title>
 </head>
 <body>
-    <div style="justify-content: center;">
-        Enter Employee Details<br><br>    
-        Name:<input type="text" placeholder="Name" name="ename"><br>
-        Phone: <input type="number" placeholder="Phone Number" name="phone"><br>
-        : </b><input type="text" placeholder="Author" name="author"><br><br>
-
-        <select name="pets" id="pet-select">
-            
-        </select>
-        <?php 
-                $sqli =  "select * from roles"; 
-                $result = mysqli_query($conn, $sqli);
-                echo $result;
+    
+    <form>
+        Enter Employee Details 
+        Name:<input type="text" placeholder="Name" name="ename">
+        Phone:<input type="number" placeholder="Phone Number" name="phone">
+        Role:<select name="role">
+            <?php
+                $sql="select * from roles";
+                if($result=mysqli_query($conn,$sql))
+                {
+                    while($row=mysqli_fetch_row($result))
+                    {
+                        echo "<option value=$row[0]>$row[1]</option>"
+                    }
+                }
             ?>
-            
-        <input type="submit">
-    </div>
+            </select>
+    </form>
 
 </body>
 </html>
